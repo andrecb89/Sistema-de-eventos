94cf46df76d7f454acaf865b37b8260510d28fe8
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-12-24 05:57:12 -0300

    Project init

---
+++ ms-ticket-manager/src/main/java/com/demo/Sistema/de/Eventos/SistemaDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Sistema.de.Eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEventosApplication.class, args);
+	}
+
+}
